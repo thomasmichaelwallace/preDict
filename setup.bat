2a74689aa6ee7b2fa86847a58525d0dc1b816234
thomas michael wallace <thomasmichaelwallace@googlemail.com>
2012-11-18 22:32:37 +0000

    initial commit

---
+++ setup.bat
@@ -0,0 +1,41 @@
+@ECHO OFF
+
+ECHO preDict setup and upgrade script
+ECHO ================================
+ECHO.
+ECHO This script will setup or upgrade preDict on your machine.
+
+PAUSE
+ECHO.
+
+ECHO Gathering information...
+
+	REM enviornmental variables	
+	SET data_path=%APPDATA%\Microsoft\Word\STARTUP\
+	
+ECHO Installing latest version...
+
+	MKDIR %data_path% >NUL 2>NUL
+	COPY "preDict.dotm" "%data_path%" >NUL 2>NUL
+
+	REM report errors
+	IF ERRORLEVEL = 1 GOTO insFail
+
+	
+:insPass
+
+ECHO.
+ECHO Setup completed successfully.
+	PAUSE
+
+	GOTO insEnd
+
+:insFail
+
+ECHO.
+ECHO Setup failed, try again or attempt manually.
+	PAUSE
+
+	GOTO insEnd
+
+:insEnd
